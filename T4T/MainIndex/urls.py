4ea229edffdc7240e6707fccbb7cb9c55da347cf
trippybruh <104927119+trippybruh@users.noreply.github.com>
2022-09-30 20:04:52 +0200

    Delete T4T directory

--- T4T/MainIndex/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.BaseIndex),
-    path('signup/', views.new_User),
-    path('login/', views.existing_User),
-]
