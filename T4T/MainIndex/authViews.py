2ec7e40481386e59036c3bbc65c1e9371835334c
trippybruh <104927119+trippybruh@users.noreply.github.com>
2022-11-02 09:35:36 +0100

    Delete T4T directory

--- T4T/MainIndex/authViews.py
+++
@@ -1,6 +0,0 @@
-from django.template import loader
-from django.urls import reverse
-from django.http import *
-from django.views.decorators.csrf import csrf_exempt, requires_csrf_token, csrf_protect
-from django.db.models import Q
-from django.contrib.auth import *
